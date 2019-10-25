828fcfab7647a37925a4b89cbe0ead84a3826a2e
Felipe  Nascimento <felipe.nascimento@certibio.com.br>
2019-10-25 18:16:13 -0300

    * Exclusão de Hoteis da Lista

--- app/src/main/java/com/example/fragments/HotelListView.kt
+++ app/src/main/java/com/example/fragments/HotelListView.kt
@@ -3,4 +3,8 @@ package com.example.fragments
 interface HotelListView{
     fun showHotels(hotels: List<Hotel>)
     fun showHotelDetails(hotel: Hotel)
+    fun showDeleteMode()
+    fun hideDeleteMode()
+    fun showSelectedHotels(hotels: List<Hotel>)
+    fun updateSelectionCountText(count: Int)
 }
